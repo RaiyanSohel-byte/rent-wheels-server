57430cd0fbaeaa0d788b65a8f29b6b1286f5b6e0
RaiyanSohel-byte <afnan19262@gmail.com>
2025-11-08 20:09:32 +0600

    middlewares not working bug fixed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.lyk9xse.mongodb.net/?appName=Cluster0`;
@@ -60,15 +62,12 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Mongo DB Connected Successfully");
   } finally {
   }
 }
 run().catch(console.dir);
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("RentWheels Server is running");
